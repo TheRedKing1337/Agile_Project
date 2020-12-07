e6ecbd86e08eff3c82227cb5090c6ce373dff4cf
Rik van Saane <72133509+TheRedKing1337@users.noreply.github.com>
2020-12-07 13:45:43 +0100

    added obstacle tracking
    obstacle bool[,] that keeps track of where obstacles are and made sectionLength dynamic

--- Assets/Scripts/Controllers/WorldBendController.cs
+++ Assets/Scripts/Controllers/WorldBendController.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class WorldBendController : MonoBehaviour
 {
-    [Range(-0.01f,0.01f)]
+    [Range(-0.01f, 0.01f)]
     public float bendX;
     [Range(-0.01f, 0.01f)]
     public float bendY;
 
-    private void OnValidate()
+    private void UpdateShaderValues()
     {
         Shader.SetGlobalFloat("_CurveX", bendX);
         Shader.SetGlobalFloat("_CurveY", bendY);
+    }
+    private void OnValidate()
+    {
+        UpdateShaderValues();
+    }
+    [ContextMenu("Set to zero")]
+    private void SetToZero()
+    {
+        bendX = 0;
+        bendY = 0;
+        UpdateShaderValues();
+    }
+    [ContextMenu("Set to default")]
+    private void SetToDefault()
+    {
+        bendX = -0.00301f;
+        bendY = -0.0019f;
+        UpdateShaderValues();
     }
 }
--- Assets/Scripts/Controllers/WorldController.cs
+++ Assets/Scripts/Controllers/WorldController.cs
@@ -7,18 +7,21 @@ public class WorldController : MonoBehaviour
     public float moveSpeed = 1;
     public float pathWidth = 3;
     public int sectionsToLoad = 3;
+    public int sectionLength = 10;
 
     private bool[,] obstacles;
+    private float distance;
+    private int obstaclePosition;
     private List<Transform> toMove = new List<Transform>();
 
     // Start is called before the first frame update
     void Start()
     {
-        obstacles = new bool[3, 10 * sectionsToLoad];
+        obstacles = new bool[3, sectionLength * sectionsToLoad];
 
         for (int i = 0; i < sectionsToLoad; i++)
         {
-            SpawnWorldTest(i * 10 * pathWidth);
+            SpawnWorldTest(i * sectionLength * pathWidth);           
         }
 
         StartCoroutine(ArrayPosCounter());
@@ -30,9 +33,11 @@ public class WorldController : MonoBehaviour
         //if player is alive
         if (true)
         {
+            //if any objects active in scene
             for (int i = 0; i < toMove.Count; i++)
             {
-                if (toMove[i].position.z < -30)
+                //if object far enough behind player
+                if (toMove[i].position.z < -sectionLength * pathWidth)
                 {
                     WorldObjectPool.Instance.Return(toMove[i].GetComponent<WorldObject>());
                     toMove.RemoveAt(i);
@@ -50,8 +55,12 @@ public class WorldController : MonoBehaviour
         //while player is alive
         while (true)
         {
+            //distance += Time.deltaTime * moveSpeed;
+            //obstaclePosition = Mathf.RoundToInt(distance/ 3 % sectionsToLoad * 10);
+            //Debug.Log(distance + " --- " + obstaclePosition);
+
             //if final object is within the new spawn range
-            if (toMove[toMove.Count - 1].position.z < (sectionsToLoad - 2) * 10 * pathWidth)
+            if (toMove[toMove.Count - 1].position.z < (sectionsToLoad - 2) * sectionLength * pathWidth)
             {
                 //spawn new section behind final object
                 SpawnWorldTest(toMove[toMove.Count - 1].position.z);
@@ -62,33 +71,57 @@ public class WorldController : MonoBehaviour
     //all dumb temp code for testing assets and pooling
     private void SpawnWorldTest(float zOffset)
     {
-        for (int z = 0; z < 10; z++)
+        //move obstacle offset to new position
+        obstaclePosition += sectionLength;
+        if (obstaclePosition > obstacles.GetLength(1) - 1)
         {
-            //spawn obstacles
+            obstaclePosition = 0;
+        }
+        //clear old obstacle area
+        for (int x = 0; x < obstacles.GetLength(0); x++)
+        {
+            for (int y = 0; y < sectionLength; y++)
+            {
+                obstacles[x, obstaclePosition + y] = false;
+            }
+        }
+        Debug.Log(obstaclePosition + "-" + (obstaclePosition + sectionLength-1) + "/" + obstacles.GetLength(1));
+
+        for (int z = 0; z < sectionLength; z++)
+        {
+            //spawn obstacles for each lane
             for (int x = 0; x < obstacles.GetLength(0); x++)
             {
-                //1 in x
-                if (Random.Range(0, 8) == 0)
+                //skip first
+                if (z != 0)
                 {
-                    int randomIndex = (Random.Range(0,2)==0)?1:3;
-                    Vector3 spawnPos = new Vector3(x * pathWidth - pathWidth, 0, z * pathWidth + zOffset);
-                    WorldObject wo = WorldObjectPool.Instance.Get(randomIndex);
-                    toMove.Add(wo.transform);
-                    wo.transform.position = spawnPos;
-                    wo.gameObject.SetActive(true);
+                    //1 in x
+                    if (Random.Range(0, 8) == 0)
+                    {
+                        //first check for space in obstacles array
+                        if (obstacles[x, obstaclePosition + z] != false) { Debug.Log("No space for new obstacle"); continue; }
 
-                    obstacles[x, z] = true;
+                        int randomIndex = (Random.Range(0, 2) == 0) ? 1 : 3;
+                        Vector3 spawnPos = new Vector3(x * pathWidth - pathWidth, 0, z * pathWidth + zOffset);
+                        WorldObject wo = WorldObjectPool.Instance.Get(randomIndex);
+                        toMove.Add(wo.transform);
+                        wo.transform.position = spawnPos;
+                        wo.gameObject.SetActive(true);
+
+                        //fill obstacles array, in real situation fill positions in front aswell
+                        obstacles[x, obstaclePosition + z] = true;
+                    }
                 }
             }
             //spawn floor
             toMove.Add(WorldObjectPool.Instance.Get((int)WorldObjectType.Floor).transform);
             toMove[toMove.Count - 1].position = new Vector3(0, 0, z * pathWidth + zOffset);
             toMove[toMove.Count - 1].gameObject.SetActive(true);
-            //spawn walls
+            //spawn walls, right
             toMove.Add(WorldObjectPool.Instance.Get((int)WorldObjectType.Wall).transform);
             toMove[toMove.Count - 1].position = new Vector3(5.5f, 0, z * pathWidth + zOffset);
             toMove[toMove.Count - 1].gameObject.SetActive(true);
-
+            //and left
             toMove.Add(WorldObjectPool.Instance.Get((int)WorldObjectType.Wall).transform);
             toMove[toMove.Count - 1].position = new Vector3(-5.5f, 0, z * pathWidth + zOffset);
             toMove[toMove.Count - 1].gameObject.SetActive(true);
