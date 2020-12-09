b2a475ef5d92d88282adc1778ced41a74080a0b6
Rik van Saane <72133509+TheRedKing1337@users.noreply.github.com>
2020-12-09 12:03:44 +0100

    Added Bend size parameter

--- Assets/Scripts/Controllers/WorldBendController.cs
+++ Assets/Scripts/Controllers/WorldBendController.cs
@@ -8,11 +8,14 @@ public class WorldBendController : MonoBehaviour
     public float bendX;
     [Range(-0.01f, 0.01f)]
     public float bendY;
+    [Range(0, 0.2f)]
+    public float bendXSize;
 
     private void UpdateShaderValues()
     {
         Shader.SetGlobalFloat("_CurveX", bendX);
         Shader.SetGlobalFloat("_CurveY", bendY);
+        Shader.SetGlobalFloat("_CurveXSize", bendXSize);
     }
     private void OnValidate()
     {
@@ -23,6 +26,7 @@ public class WorldBendController : MonoBehaviour
     {
         bendX = 0;
         bendY = 0;
+        bendXSize = 0;
         UpdateShaderValues();
     }
     [ContextMenu("Set to default")]
@@ -30,6 +34,7 @@ public class WorldBendController : MonoBehaviour
     {
         bendX = -0.00301f;
         bendY = -0.0019f;
+        bendXSize = 0.1f;
         UpdateShaderValues();
     }
 }
